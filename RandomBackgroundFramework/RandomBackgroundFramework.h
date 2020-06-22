74ee907fc66af3d53335a531ca1e0a8267a3229b
Jaeseob Hwang <jetsbee@gmail.com>
2020-06-22 18:22:49 +0900

    Initial Commit

---
+++ RandomBackgroundFramework/RandomBackgroundFramework.h
@@ -0,0 +1,19 @@
+//
+//  RandomBackgroundFramework.h
+//  RandomBackgroundFramework
+//
+//  Created by jaeseob Hwang on 2020/06/22.
+//  Copyright © 2020 jaeseob Hwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RandomBackgroundFramework.
+FOUNDATION_EXPORT double RandomBackgroundFrameworkVersionNumber;
+
+//! Project version string for RandomBackgroundFramework.
+FOUNDATION_EXPORT const unsigned char RandomBackgroundFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomBackgroundFramework/PublicHeader.h>
+
+
